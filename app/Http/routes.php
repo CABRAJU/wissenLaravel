97243fad8a348e97eb5f8cbc439adb7ac1d0605a
Brandon Sandoval <brandon.sand11@hotmail.com>
2015-05-29 10:54:05 -0500

    CABRAJU ;)

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -17,7 +17,17 @@ Route::get('home', 'HomeController@index');
 
 Route::controller('entidades', 'EntidadesController');
 Route::controller('disciplinas', 'DisciplinasController');
+Route::controller('disciplinas_traduc', 'Disciplinas_traducController');
+Route::controller('niveles', 'NivelesController');
+Route::controller('niveles_traduc', 'Niveles_traducController');
+Route::controller('categorias', 'CategoriasController');
+Route::controller('categorias_traduc', 'CategoriasController');
 Route::controller('eventos', 'EventosController');
+Route::controller('nivel_participantes', 'Nivel_participantesController');
+Route::controller('inscripciones', 'InscripcionesController');
+Route::controller('user_event', 'User_eventController');
+Route::controller('evaluaciones', 'EvaluacionesController');
+Route::controller('preguntas_evaluacion', 'Preguntas_evaluacionController');
 
 
 
